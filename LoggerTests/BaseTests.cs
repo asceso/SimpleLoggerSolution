0a7cacd2892e57cd6bc811b500f1f371f30e201c
Uchidero Krauch <asceso111@gmail.com>
2022-02-04 12:32:36 +0300

    add file logger

--- LoggerTests/BaseTests.cs
+++ LoggerTests/BaseTests.cs
@@ -1,20 +1,26 @@
 using NUnit.Framework;
 using SimpleLogger.ConsoleService;
+using SimpleLogger.FileService;
+using System;
+using System.Collections.Generic;
 
 namespace LoggerTests
 {
     public class Tests
     {
         IConsoleLogger logger;
+        IFileLogger fileLogger;
+        int bulkTestCount = 10000;
 
         [SetUp]
         public void Setup()
         {
             logger = new ConsoleLogger();
+            fileLogger = new FileLogger();
         }
 
         [Test]
-        public void TestAllMethods()
+        public void TestConsoleLogger()
         {
             logger.Info("Test info message");
             logger.Warning("Test warning message");
@@ -27,6 +33,64 @@ namespace LoggerTests
             logger.Warning("Test warning message");
             logger.Error("Test error message");
             logger.Fatal("Test fatal message");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestFileLogger()
+        {
+            fileLogger.InitLogsFolder();
+            fileLogger.Info("Test info message");
+            fileLogger.Warning("Test warning message");
+            fileLogger.Error("Test error message");
+            fileLogger.Fatal("Test fatal message");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestFileLoggerWithBulkData()
+        {
+            List<string> randomWords = new List<string>()
+            {
+                "Гнездиться",
+                "Девятисотлетний",
+                "Мрачный",
+                "Надрать",
+                "Начатки",
+                "Непреложный",
+                "Откуда",
+                "Полосчатый",
+                "Резь",
+                "Шип",
+                "Венчаться",
+                "Гербарий",
+                "Дрель",
+                "Запустение",
+                "Зоосад",
+                "Лесостепь",
+                "Огневой",
+                "Растяжимый",
+                "Умственный",
+                "Фуражка"
+            };
+
+            fileLogger.InitLogsFolder();
+            Random random = new Random();
+            for (int i = 0; i < bulkTestCount; i++)
+            {
+                int firstWordIndex = random.Next(randomWords.Count);
+                int secondWordIndex = random.Next(randomWords.Count);
+
+                string message = $"{randomWords[firstWordIndex]} {randomWords[secondWordIndex]}";
+                int level = random.Next(0, 4);
+                switch (level)
+                {
+                    case 0: fileLogger.Info(message); break;
+                    case 1: fileLogger.Warning(message); break;
+                    case 2: fileLogger.Error(message); break;
+                    case 3: fileLogger.Fatal(message); break;
+                }
+            }
             Assert.Pass();
         }
     }
