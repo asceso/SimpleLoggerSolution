82222f067a59592a762dd677f10bcbe0e30b64b6
Uchidero Krauch <asceso111@gmail.com>
2022-02-05 15:34:18 +0300

    add common service

---
+++ SimpleLogger/CommonService/ISimpleLogger.cs
@@ -0,0 +1,35 @@
+﻿using SimpleLogger.ConsoleService;
+using SimpleLogger.FileService;
+
+namespace SimpleLogger.CommonService
+{
+    public interface ISimpleLogger
+    {
+        /// <summary>
+        /// Method set inited logger interfaces
+        /// </summary>
+        /// <param name="consoleLogger">console logger interface</param>
+        /// <param name="fileLogger">file logger interface</param>
+        public void SetLoggerInterfaces(IConsoleLogger consoleLogger, IFileLogger fileLogger);
+        /// <summary>
+        /// Show info in console and write to file
+        /// </summary>
+        /// <param name="message">Message text</param>
+        public void Info(string message);
+        /// <summary>
+        /// Show warning in console and write to file
+        /// </summary>
+        /// <param name="message">Message text</param>
+        public void Warning(string message);
+        /// <summary>
+        /// Show error in console and write to file
+        /// </summary>
+        /// <param name="message">Message text</param>
+        public void Error(string message);
+        /// <summary>
+        /// Show fatal in console and write to file
+        /// </summary>
+        /// <param name="message">Message text</param>
+        public void Fatal(string message);
+    }
+}
---
+++ SimpleLogger/CommonService/SimpleLogger.cs
@@ -0,0 +1,37 @@
+﻿using SimpleLogger.ConsoleService;
+using SimpleLogger.FileService;
+
+namespace SimpleLogger.CommonService
+{
+    public class SimpleLogger : ISimpleLogger
+    {
+        private IConsoleLogger consoleLogger;
+        private IFileLogger fileLogger;
+
+        public void SetLoggerInterfaces(IConsoleLogger consoleLogger, IFileLogger fileLogger)
+        {
+            this.consoleLogger = consoleLogger;
+            this.fileLogger = fileLogger;
+        }
+        public void Info(string message)
+        {
+            consoleLogger.Info(message);
+            fileLogger.Info(message);
+        }
+        public void Warning(string message)
+        {
+            consoleLogger.Warning(message);
+            fileLogger.Warning(message);
+        }
+        public void Error(string message)
+        {
+            consoleLogger.Error(message);
+            fileLogger.Error(message);
+        }
+        public void Fatal(string message)
+        {
+            consoleLogger.Fatal(message);
+            fileLogger.Fatal(message);
+        }
+    }
+}
